4ddf61ccc2016db8f070e8b324a79f3abbf3f6a8
IgorLugovoiii <qeadzc4065@gmail.com>
2025-04-14 22:41:05 +0300

    Added tests in notification-service

---
+++ modules/notification-service/src/test/java/com/example/notification_service/services/NotificationServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.notification_service.services;
+
+import com.example.notification_service.dtos.NotificationRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class NotificationServiceTest {
+    @Mock
+    private JavaMailSender javaMailSender;
+    @InjectMocks
+    private NotificationService notificationService;
+
+    @Test
+    void testSendNotification_shouldSendEmail() {
+        NotificationRequest notificationRequest = new NotificationRequest(
+                "test@example.com",
+                "Test Subject",
+                "Hello from test!"
+        );
+
+        notificationService.sendNotification(notificationRequest);
+
+        ArgumentCaptor<SimpleMailMessage> messageCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
+        verify(javaMailSender, times(1)).send(messageCaptor.capture());
+
+        SimpleMailMessage sentMessage = messageCaptor.getValue();
+        assertNotNull(sentMessage.getTo());
+        assertEquals("test@example.com", sentMessage.getTo()[0]);
+        assertEquals("Test Subject", sentMessage.getSubject());
+        assertEquals("Hello from test!", sentMessage.getText());
+    }
+
+    @Test
+    void testSendNotification_shouldHandleException() {
+        NotificationRequest request = new NotificationRequest(
+                "fail@example.com",
+                "Fails",
+                "Expect exception"
+        );
+
+        doThrow(new RuntimeException("Mail server error")).when(javaMailSender).send(any(SimpleMailMessage.class));
+
+        assertThrows(RuntimeException.class, () -> notificationService.sendNotification(request));
+
+        verify(javaMailSender, times(1)).send(any(SimpleMailMessage.class));
+    }
+}
