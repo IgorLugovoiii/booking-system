a75e391cbc02fd55e3eb025026ee91107f2a88a7
IgorLugovoiii <qeadzc4065@gmail.com>
2026-04-06 22:33:26 +0300

    Fixed some bugs and replaced jwt-auth with Keycloak(OAuth2 + OIDC)

--- modules/api-gateway/src/main/java/com/example/api_gateway/configs/SecurityConfig.java
+++ modules/api-gateway/src/main/java/com/example/api_gateway/configs/SecurityConfig.java
@@ -1,35 +1,30 @@
 package com.example.api_gateway.configs;
 
-import com.example.api_gateway.filters.JwtAuthenticationFilter;
-import com.example.api_gateway.filters.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
-                        .pathMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .pathMatchers("/actuator/prometheus").permitAll()
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/error").permitAll()
                         .anyExchange().authenticated()
                 )
-                .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .oauth2ResourceServer(oauth2-> oauth2
+                        .jwt(
+                                jwtSpec -> jwtSpec //змінити реалм
+                                        .jwkSetUri("http://keycloak:8080/realms/booking-system/protocol/openid-connect/certs")
+                        ))
                 .build();
     }
 }
--- modules/auth-service/src/main/java/com/example/auth_service/models/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.auth_service.models.enums;
-
-public enum Role {
-    USER, ADMIN
-}
